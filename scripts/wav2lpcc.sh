b033a4079d7c643143ab71e5ccb1c48aa8db9ef3
Stephanie Salas <A00835015@tec.mx>
2025-01-05 22:44:41 +0100

    Ex

--- scripts/wav2lpcc.sh
+++ scripts/wav2lpcc.sh
@@ -51,11 +51,7 @@ sox $inputfile -t raw -e signed -b 16 - | $X2X +sf | $FRAME -l 240 -p 80 | $WIND
    
 
 # Our array files need a header with the number of cols and rows:
-<<<<<<< HEAD
-ncol=$((lpc_order + 1)) # lpc p =>  (gain a1 a2 ... ap) 
-=======
 ncol=$((lpc_order + 1)) # lpc p =>  (gain a1 a2 ... ap) tambien cambiara en las otras
->>>>>>> e2b3d4f715413f50c42c558021627e00622fdfbe
 nrow=$(($($X2X +fa < $base.lp | wc -l) / ncol))
 
 # Build fmatrix file by placing nrow and ncol in front, and the data after them
